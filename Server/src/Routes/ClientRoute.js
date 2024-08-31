3347c76365843aafd2bb229ab949ed3f65b81cd0
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2024-08-31 13:19:13 +0800

    added api route for add transaction

--- Server/src/Routes/ClientRoute.js
+++ Server/src/Routes/ClientRoute.js
@@ -3,9 +3,24 @@ const router = express.Router();
 const connection = require("../Config/DatabaseConfig");
 
 router.get("/api/transaction/list", (req, res) => {
-    connection.query("SELECT * FROM client_transaction", (err, result) => {
+    const query = "SELECT * FROM client_transaction";
+    connection.query(query, (err, result) => {
         if (err) {
             return res.status(500).json({ error: "Failed to retrieve data" });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+router.post("/api/add/transaction", (req, res) => {
+    const query = "INSERT INTO client_transaction (name, number, type, kilo, price, transaction_date, date_received) VALUES (?,?,?,?,?,?,?)";
+    const {name, number, type, kilo, price, transaction_date, date_received} = req.body;
+    const values = [name, number, type, kilo, price, transaction_date, date_received];
+
+    connection.query(query, values, (err, result) => {
+        if(err) {
+            return res.status(500).json({ error: "Failed to add data" });
         } else {
             res.json(result);
         }
