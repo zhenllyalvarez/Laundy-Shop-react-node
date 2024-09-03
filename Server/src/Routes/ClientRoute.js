4647215f69f694c82c1b0426682be0afc0eff209
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2024-09-03 13:44:43 +0800

    added api

--- Server/src/Routes/ClientRoute.js
+++ Server/src/Routes/ClientRoute.js
@@ -2,15 +2,30 @@ const express = require('express');
 const router = express.Router();
 const connection = require("../Config/DatabaseConfig");
 
-router.get("/api/transaction/list", (req, res) => {
-    const query = "SELECT * FROM client_transaction";
-    connection.query(query, (err, result) => {
+// Fetch transactions with a specific status
+router.get("/api/customer/transaction/list", (req, res) => {
+    const status = req.query.status || 0; // Default to 0 if no status is provided
+    const query = "SELECT * FROM client_transaction WHERE status = ?";
+    
+    connection.query(query, [status], (err, result) => {
         if (err) {
             return res.status(500).json({ error: "Failed to retrieve data" });
         } else {
             res.json(result);
         }
     });
+});
+
+router.get('/api/transaction/list', (req, res) => {
+    const query = "SELECT * FROM client_transaction WHERE status = ?";
+    const status = req.query.status || 1;
+    connection.query(query, [status], (err, result) => {
+        if(err) {
+            return res.status(500).json({ error: "Failed to retrieve data"});
+        } else {
+            res.json(result);
+        }
+    })
 });
 
 router.post("/api/add/transaction", (req, res) => {
@@ -25,6 +40,19 @@ router.post("/api/add/transaction", (req, res) => {
             res.json(result);
         }
     });
+});
+
+router.put("/api/transaction/update/:id", (req, res) => {
+    const query = "UPDATE client_transaction SET status = ? WHERE id = ?";
+    const id = req.params.id;
+    const { status } = req.body;
+    connection.query(query, [status, id ], (err, result)=> {
+        if(err) {
+            return res.status(500).json({ error: "Failed to update data"});
+        } else {
+            res.json(result);
+        }
+    })
 });
 
 module.exports = router;
